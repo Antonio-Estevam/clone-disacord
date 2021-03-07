a303a8e9f3275faeda84db3208fa0b5ea681c13d
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2021-03-07 18:07:08 -0300

    Server List

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalsStyles';
+
+function App() {
+  return (
+    <>
+
+    <Layout/>
+
+    <GlobalStyles />
+
+    </>
+  );
+}
+
+export default App;
