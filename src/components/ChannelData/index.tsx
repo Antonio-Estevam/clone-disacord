971767cd38aadac2d966dca122a083d5c024a9f4
antonio-estevam <jhonnyimmbe@gmail.com>
2021-03-14 14:34:49 -0300

    final

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import ChannelMessage,{Mention} from '../ChannelMessage';
 
 import { Container, Messages, InputWrapper, Input, InputIcone } from './styles';
 
 const ChannelData: React.FC = () => {
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+    useEffect(() => {
+        const div = messagesRef.current;
+
+        if(div){
+            div.scrollTop = div.scrollHeight;
+        }
+    }, [messagesRef]);
+
     return (
         <Container>
-            <Messages/>
+            <Messages ref={messagesRef}>
+
+                {Array.from(Array(10).keys()).map((n) =>(
+                    <ChannelMessage
+                    key={n}
+                    author="Antonio Estevam Filho"
+                    date="22/10/2021"
+                    content="Hoje é o meu aniversário"
+                />
+                ))}
+
+
+                <ChannelMessage
+                    author="Lucas Silva "
+                    date="22/10/2021"
+                    content={
+                        <>
+                            <Mention>@Antonio</Mention> vamos jogar?
+                        </>                        
+                    }
+                    hasMention
+                    isBot
+                />
+
+            </Messages>
 
             <InputWrapper>
 
-                <Input />
+                <Input type="text" placeholder="Conversar em #chate-liver" />
                 
                 <InputIcone/>
 
@@ -17,5 +52,5 @@ const ChannelData: React.FC = () => {
         </Container>
     );
 };
-
+//https://youtu.be/x4FdZd2-_uU?t=8503
 export default ChannelData;
